006f683d575d9635aea4c87c79a0d71f109c443d
Erin McKinney <e.mckinney605@gmail.com>
2021-08-03 21:22:33 -0400

    Add project files.

---
+++ SQLChallenges/BonusChallenge/ProfessorCourse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLChallenges.BonusChallenge
+{
+    public class ProfessorCourse
+    {
+        public int ProfessorId { get; set; }
+        public Professor Professor { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
